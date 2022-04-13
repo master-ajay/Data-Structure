b71977252d8c82a24c52995a5ac27c2264cefd9a
Ajay Rawat <48316457+git-projects-creator@users.noreply.github.com>
2022-04-13 16:43:39 +0530

    Update Rotate array.cpp

--- Rotate array.cpp
+++ Rotate array.cpp
@@ -1,4 +1,4 @@
-//Time Complexity O(n) , Space Complexity O(1) Juggling Algorithm
+//Time Complexity O(n) , Space Complexity O(1)
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -8,48 +8,72 @@ void printArray(int arr[], int size)
         cout << arr[i] << " ";
 }
 
-int gcd(int a,int b){
-    if(b==0)
-        return a;
-    else
-        return gcd(b,a%b);
+void reverse(int arr[],int low,int high){
+    while(low<high)
+    {
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
+    } 
 }
-
-void leftRotate(int arr[],int d,int n){
-    
-    d=d%n;
-    int g_c_d = gcd(d,n);
-    
-    for(int i=0;i<g_c_d;i++){
-        
-        int temp=arr[i];
-        int j=i;
-        
-        while(1){
-            int k=j+d;
-            if(k>=n)
-                k=k-n;
-            
-            if(k==i)
-                break;
-
-            arr[j] = arr[k];
-            j = k;
-        }
-        arr[j] = temp;
-    }
-}
-
 
 int main(){
+ 
+   int arr[]={1,2,3,4,5};
+   int leftrotateby=2;
+   
+   int len=sizeof(arr)/sizeof(arr[0]);
+   leftrotateby=leftrotateby%len;
+   
+   reverse(arr,0,leftrotateby-1);
+   reverse(arr,leftrotateby,len-1);
+   reverse(arr,0,len-1);
     
-    int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    leftRotate(arr, 2, n);
-    printArray(arr, n);
-    
-    return 0;
+   printArray(arr,len);
 }
+ 
+// int gcd(int a,int b){
+//     if(b==0)
+//         return a;
+//     else
+//         return gcd(b,a%b);
+// }
+
+// void leftRotate(int arr[],int d,int n){
+    
+//     d=d%n;
+//     int g_c_d = gcd(d,n);
+    
+//     for(int i=0;i<g_c_d;i++){
+        
+//         int temp=arr[i];
+//         int j=i;
+        
+//         while(1){
+//             int k=j+d;
+//             if(k>=n)
+//                 k=k-n;
+            
+//             if(k==i)
+//                 break;
+
+//             arr[j] = arr[k];
+//             j = k;
+//         }
+//         arr[j] = temp;
+//     }
+// }
+
+
+// int main(){
+    
+//     int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+//     int n = sizeof(arr) / sizeof(arr[0]);
+//     leftRotate(arr, 2, n);
+//     printArray(arr, n);
+    
+//     return 0;
+// }
 
 
 
