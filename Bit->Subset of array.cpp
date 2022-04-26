e48f5f81ffcc114542b71ffb94f2872ada939b1c
Ajay Rawat <48316457+git-projects-creator@users.noreply.github.com>
2022-04-26 22:32:48 +0530

    Create Bit->Subset of array.cpp

---
+++ Bit->Subset of array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void subset(int arr[],int n){
+    for(int i=0;i<(1<<n);i++){
+        for(int j=0;j<n;j++){
+            if(i&(1<<j))
+                cout<<arr[j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int arr[]={1,2,3,4};
+    subset(arr,4);
+}
