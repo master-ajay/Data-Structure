33fde07db1826e94f5363c156cc330eb239bf835
Ajay Rawat <48316457+git-projects-creator@users.noreply.github.com>
2022-05-06 02:18:13 +0530

    Create Count the number of all path from start point to end point in gameboard.cpp

---
+++ Count the number of all path from start point to end point in gameboard.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countPaths(int s,int e){
+    if(s==e){
+        return 1;
+    }
+    if(s>e){
+        return 0;
+    }
+    
+    int count=0;
+    for(int i=1;i<=6;i++){
+        count+=countPaths(s+i,e);
+    }
+    return count;
+}
+
+int main()
+{
+    cout<<countPaths(1,5);
+    return 0;
+}
